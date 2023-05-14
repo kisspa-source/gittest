3a2f2bb4ddac488a0dc17f3d24f431706e3e747b
jaehyoungpark <kisspa@gmail.com>
2023-05-14 22:02:13 +0900

    main에서 commit 한 내용입니다.

--- test/test_js.js
+++ test/test_js.js
@@ -2,15 +2,17 @@ let person;
 
 console.log(person);
 
-person.member = {};
-person.add = function (id = '1', name='jh.park', age=20) {
-    let member;
-    member.add(id, name, age)
-};
-
-
-console.log(person);
-
 console.log('test');
 
 console.log('드디어 원격에....')
+console.log('이건 main에서 작업한 내용이야');
+
+var name = function () {
+    console.log('ddd');
+};
+
+name();
+
+for (const nameKey in name) {
+    console.log(nameKey);
+}
