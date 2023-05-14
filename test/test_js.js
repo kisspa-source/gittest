e4ce481757ced2dae4071c79b540b7b237ee6441
jaehyoungpark <kisspa@gmail.com>
2023-05-14 21:45:45 +0900

    test

--- test/test_js.js
+++ test/test_js.js
@@ -1,11 +1,12 @@
-var person;
+let person;
 
-person.name = 'TestId';
+console.log(person);
+
 person.member = {};
 person.add = function (id = '1', name='jh.park', age=20) {
-    member.add(id, name, age);
+    let member;
+    member.add(id, name, age)
 };
 
-person.add();
 
 console.log(person);
